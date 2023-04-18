5397ccfd2ef849bd51827ade7635572826cdfebb
Prachiti Chandratreya <prachitichandratreya@gmail.com>
2023-04-19 01:10:36 +0530

    Created view using ListAPI

--- sponsors/views.py
+++ sponsors/views.py
@@ -3,15 +3,12 @@ from .models import Sponsor
 from .serializers import SponsorSerializer
 from rest_framework.generics import ListAPIView
 # from django.db.models import Prefetch
-from rest_framework.response import Response
+# from rest_framework.response import Response
 
 
 class SponsorView(ListAPIView):
-    def get(self, request):
-        permission_classes = [permissions.AllowAny]
-        # serializer_class = SponsorSerializer
-        model = Sponsor
-        queryset = Sponsor.objects.all()
-        serializer = SponsorSerializer(queryset, many=True)
-        return Response({"data": serializer.data})
-
+    permission_classes = [permissions.AllowAny]
+    serializer_class = SponsorSerializer
+    model = Sponsor
+    queryset = Sponsor.objects.all()
+    # serializer = SponsorSerializer(queryset, many=True)
